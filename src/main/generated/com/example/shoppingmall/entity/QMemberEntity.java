edf158b9a29810d05960ed9031eb722a61895666
YuYoHan <zxzz8014@naver.com>
2023-05-04 20:02:26 +0900

    [Add] security 설정 추가

---
+++ src/main/generated/com/example/shoppingmall/entity/QMemberEntity.java
@@ -0,0 +1,47 @@
+package com.example.shoppingmall.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QMemberEntity is a Querydsl query type for MemberEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QMemberEntity extends EntityPathBase<MemberEntity> {
+
+    private static final long serialVersionUID = -618143801L;
+
+    public static final QMemberEntity memberEntity = new QMemberEntity("memberEntity");
+
+    public final StringPath addr = createString("addr");
+
+    public final StringPath email = createString("email");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath password = createString("password");
+
+    public final EnumPath<com.example.shoppingmall.constant.Role> role = createEnum("role", com.example.shoppingmall.constant.Role.class);
+
+    public QMemberEntity(String variable) {
+        super(MemberEntity.class, forVariable(variable));
+    }
+
+    public QMemberEntity(Path<? extends MemberEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QMemberEntity(PathMetadata metadata) {
+        super(MemberEntity.class, metadata);
+    }
+
+}
+
