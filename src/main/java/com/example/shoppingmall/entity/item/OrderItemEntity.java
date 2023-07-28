d01a5953eb8b1617bee6e2afa61f2663dcacb7cd
YuYoHan <zxzz8014@naver.com>
2023-07-28 18:41:02 +0900

    [Study] 지연 로딩 방식

--- src/main/java/com/example/shoppingmall/entity/item/OrderItemEntity.java
+++ src/main/java/com/example/shoppingmall/entity/item/OrderItemEntity.java
@@ -20,13 +20,13 @@ public class OrderItemEntity extends BaseTimeEntity {
 
     // 하나의 상품은 여러 주문 상품으로 들어갈 수 있으므로
     // 주문 상품 기준으로 다대일 단방향 매핑을 한다.
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "item_id")
     private ItemEntity item;
 
     // 한 번의 주문에 여러 개의 상품을 주문할 수 있으므로
     // 주문 상품 엔티티와 주문 엔티티를 다대일 단방향 매핑을 한다.
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private OrderEntity order;
 
