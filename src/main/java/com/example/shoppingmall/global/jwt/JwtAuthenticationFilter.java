2cfaebf598bc959cfbcbfb272b4d4f5891293018
YuYoHan <zxzz8014@naver.com>
2024-01-17 21:00:28 +0900

    [Feat] Jwt 예외처리 기능 구현

---
+++ src/main/java/com/example/shoppingmall/global/jwt/JwtAuthenticationFilter.java
@@ -0,0 +1,12 @@
+package com.example.shoppingmall.global.jwt;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+@Log4j2
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    public static final String HEADER_AUTHORIZATION = "Authorization";
+    private final
+}
