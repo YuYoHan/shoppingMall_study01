f28f32754f091b4d7987a678d53de9f98a279691
YuYoHan <zxzz8014@naver.com>
2024-01-18 10:09:13 +0900

    [Feat] swagger 기능 구현

---
+++ src/main/java/com/example/shoppingmall/global/config/swagger/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.example.shoppingmall.global.config.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.shoopingmall.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo())
+                .useDefaultResponseMessages(false);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API 문서")
+                .description("API 문서화 페이지입니다.")
+                .version("1.0")
+                .build();
+    }
+}
