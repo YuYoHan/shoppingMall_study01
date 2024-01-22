632d4fc1f69e9a6111f8309853d7abf9584a03f6
YuYoHan <zxzz8014@naver.com>
2024-01-22 21:25:40 +0900

    [Feat] Auditing 기능 구현

---
+++ src/main/java/com/example/shoppingmall/domain/model/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.example.shoppingmall.domain.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime regTime;
+
+    // 엔티티 값을 변경할 때 시간을 자동으로 저장
+    @LastModifiedDate
+    private LocalDateTime updateTime;
+}
