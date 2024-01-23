089413be3e6d7d6acce2c5c8199e9e1fc28792d0
YuYoHan <zxzz8014@naver.com>
2024-01-23 22:19:55 +0900

    [Feat] 유저 수정 기능 구현

--- src/main/java/com/example/shoppingmall/domain/member/dto/ModifyMemberDTO.java
+++ src/main/java/com/example/shoppingmall/domain/member/dto/ModifyMemberDTO.java
@@ -1,6 +1,5 @@
 package com.example.shoppingmall.domain.member.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,15 +11,12 @@ import javax.validation.constraints.Pattern;
 @Getter
 @NoArgsConstructor
 public class ModifyMemberDTO {
-    @Schema(description = "닉네임")
     @Pattern(regexp = "^[a-zA-Z가-힣]*$", message = "사용자이름은 영어와 한글만 가능합니다.")
     private String nickName;
 
-    @Schema(description = "회원 비밀번호")
     @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*[0-9])(?=.*[$@$!%*#?&])[A-Za-z[0-9]$@$!%*#?&]{8,15}", message = "비밀번호는 영문 소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8 ~15자의 비밀번호여야 합니다." )
     private String memberPw;
 
-    @Schema(description = "회원 주소")
     private AddressDTO memberAddress;
 
     @Builder
