09e22dc33235b023f71c1b2b5012ee5f071cfc47
YuYoHan <zxzz8014@naver.com>
2024-03-05 23:43:20 +0900

    [Refactor] 레포지토리 수정

--- src/main/java/com/example/shoppingmall/domain/member/repository/SocialMemberRepository.java
+++ src/main/java/com/example/shoppingmall/domain/member/repository/SocialMemberRepository.java
@@ -1,7 +1,6 @@
 package com.example.shoppingmall.domain.member.repository;
 
 import com.example.shoppingmall.domain.member.entity.SocialMemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SocialMemberRepository extends JpaRepository<SocialMemberEntity, Long> {
     SocialMemberEntity findByEmail(String email);
